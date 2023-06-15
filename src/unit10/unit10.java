02cfec5e022286dae13e41b9baa1f3f6df726ea5
morozvitali <80890701+morozvitali@users.noreply.github.com>
2023-06-15 17:57:22 +0300

    пщ пщ

--- src/unit10/unit10.java
+++ src/unit10/unit10.java
@@ -1,7 +1,22 @@
 package unit10;
 
-import java.io.InputStream;
+import java.io.*;
 
 public class unit10 {
 
-}
+    public static void main(String[] args) {
+        try (InputStream in = new FileInputStream("test.txt")){
+            int aByte = in.read();
+            StringBuffer sb = new StringBuffer();
+
+            while (aByte != -1) {
+                aByte = in.read();
+                sb.append((char) aByte);
+            }
+            System.out.print(sb);
+        }
+        catch (FileNotFoundException e){ }
+        catch (IOException e){ }
+            throw new RuntimeException ();
+        }
+    }
